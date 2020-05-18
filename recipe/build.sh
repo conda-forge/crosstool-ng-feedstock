07ed464991ae0de225e78761e8b8db93469b17a1
Isuru Fernando <isuruf@gmail.com>
2020-05-18 04:35:45 +0000

    Workaround for ARG_MAX

--- recipe/build.sh
+++ recipe/build.sh
@@ -27,6 +27,9 @@ export PATCH="/usr/bin/env patch"
 export GPERF="/usr/bin/env gperf"
 unset ncurses
 
+mkdir tmp
+mv $SRC_DIR/packages/glibc/2.17/*-glibc-*.patch tmp
+
 export CPPFLAGS="-I${PREFIX}/include -L${PREFIX}/lib -Wl,-rpath-link,${PREFIX}/lib"
 if [[ $(uname) == Darwin ]]; then
   export DYLD_FALLBACK_LIBRARY_PATH=${PREFIX}/lib
@@ -36,3 +39,5 @@ getconf ARG_MAX
 ./configure --prefix=${PREFIX} || (cat config.log && exit 1)
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make install
+
+mv tmp/*.patch $PREFIX/share/crosstool-ng/packages/glibc/2.17/
